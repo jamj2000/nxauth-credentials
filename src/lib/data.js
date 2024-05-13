3d72af3c91098b29fb324cf149a7ab4bf233bca0
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2024-05-13 14:45:41 +0200

    Update data.js

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,4 +1,13 @@
+import { prisma } from "@/lib/prisma";
 
+export async function getUserByEmail( email ) {
+  const user = await prisma.user.findUnique({
+      where: { email }
+    });
+  //console.log(user);
+  return user
+
+}
 
 export async function getUserById( id ) {
     const user = await prisma.user.findUnique({
